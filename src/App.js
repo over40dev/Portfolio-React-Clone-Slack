f2804c242c5a80a8b2d4c73b420543cbfa563b29
Cogentx <code@cogentx.com>
2021-08-20 17:13:36 -0230

    CSS vars; styled-components; material-ui+icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's Build Slack</h1>
+      <Router>
+      <>
+        <Switch>
+          <Route path="/" exact>
+           <h1>This is the Home Page</h1>
+          </Route>
+        </Switch>
+      </>
+    </Router>
     </div>
   );
 }
