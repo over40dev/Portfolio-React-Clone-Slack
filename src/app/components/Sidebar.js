63590fa813c08356046f12e805690d93e296c97b
Cogentx <code@cogentx.com>
2021-09-14 15:38:29 -0230

    Add SidebarOptions Component

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import {useCollection} from 'react-firebase-hooks/firestore';
 import styled from 'styled-components';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
-import InsertCommentIcon from '@material-ui/icons/InsertComment';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
-import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
-import AppsIcon from '@material-ui/icons/Apps';
-import FileCopyIcon from '@material-ui/icons/FileCopy';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import AddIcon from '@material-ui/icons/Add';
-import SidebarOption from './SidebarOption';
-import {db} from '../firebase';
+import SidebarOptions from './SidebarOptions';
 
 function Sidebar() {
-  const [channels, loading, error] = useCollection(db.collection('rooms'));
 
   return (
     <SidebarContainer>
@@ -31,28 +18,7 @@ function Sidebar() {
         </SidebarInfo>
         <CreateIcon></CreateIcon>
       </SidebarHeader>
-      <SidebarOption Icon={InsertCommentIcon} title="Threads"/>
-      <SidebarOption Icon={InboxIcon} title="Mentions & Reactions"/>
-      <SidebarOption Icon={DraftsIcon} title="Saved Items"/>
-      <SidebarOption Icon={BookmarkBorderIcon} title="Channel Browser"/>
-      <SidebarOption Icon={PeopleAltIcon} title="People & User Groups"/>
-      <SidebarOption Icon={AppsIcon} title="Apps"/>
-      <SidebarOption Icon={FileCopyIcon} title="File Browser"/>
-      <SidebarOption Icon={ExpandLessIcon} title="Show Less"/>
-
-      <hr />
-      <SidebarOption Icon={ExpandMoreIcon} title="Channels"/>
-
-      <hr />
-
-      <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel"/>
-
-      {channels?.docs.map((doc) => 
-        <SidebarOption key={doc.id} 
-          title={doc.data().name}
-          id={doc.id}/>
-      )}
-    
+      <SidebarOptions/>
     </SidebarContainer>
   );
 }
@@ -71,7 +37,7 @@ const SidebarContainer = styled.div`
   > hr {
     margin-top: 10px;
     margin-bottom: 10px;
-    border: 1px solid var( --slack-color-accent);
+    border: 1px solid var(--slack-color-accent);
   }
 `;
 
