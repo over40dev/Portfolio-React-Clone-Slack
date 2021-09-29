5f220c5e2964e68c473a97d7e6f0f45a7af0569e
Cogentx <code@cogentx.com>
2021-09-29 13:08:11 -0230

    Included Spinners when in a loading state

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Spinner from 'react-spinkit';
 import './App.css';
 import styled from 'styled-components';
 import Chat from './components/Chat';
@@ -11,6 +12,20 @@ import Login from './components/Login';
 
 function App() {
   const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img
+            src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+            alt=""
+          />
+          <LoadingSpinner name="ball-spin-fade-loader" fadeIn="none" />
+        </AppLoadingContents>
+      </AppLoading>
+    );
+  }
 
   return (
     <div className="app">
@@ -40,4 +55,30 @@ export default App;
 const AppBody = styled.div`
   display: flex;
   height: 100vh;
+`;
+
+const AppLoading = styled.div`
+  display: grid;
+  height: 100vh;
+  width: 100%;
+  place-items: center;
+`;
+
+const AppLoadingContents = styled.div`
+  display: flex;
+  text-align: center;
+  padding-bottom: 100px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
+`;
+
+const LoadingSpinner = styled(Spinner)`
+  color: var(--brand-color);
 `;
--- src/app/components/SidebarOptions.js
+++ src/app/components/SidebarOptions.js
@@ -11,11 +11,26 @@ import FileCopyIcon from '@material-ui/icons/FileCopy';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
+import Spinner from 'react-spinkit';
 import SidebarOption from './SidebarOption';
 import {db} from '../firebase';
 
 function SidebarOptions() {
   const [channels, loading, error] = useCollection(db.collection('rooms'));
+
+  if (!error && loading) {
+    return (
+      <SidebarOptionsLoading>
+        <SidebarOptionsLoadingContents>
+          <img
+            src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+            alt=""
+          />
+          <LoadingSpinner name="line-scale-pulse-out" fadeIn="none" />
+        </SidebarOptionsLoadingContents>
+      </SidebarOptionsLoading>
+    );
+  }
 
   return (
     <SidebarOptionsContainer>
@@ -45,5 +60,30 @@ function SidebarOptions() {
 export default SidebarOptions;
 
 const SidebarOptionsContainer = styled.div`
-overflow-y: scroll;
+  overflow-y: scroll;
 `;
+
+const SidebarOptionsLoading = styled.div`
+display: grid;
+height: 100vh;
+width: 100%;
+place-items: center;
+background-color: white
+`;
+
+const SidebarOptionsLoadingContents = styled.div`
+display: flex;
+text-align: center;
+padding-bottom: 100px;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+>img {
+  height: 100px;
+  padding: 20px;
+  margin-bottom: 40px;
+}
+`;
+
+const LoadingSpinner = styled(Spinner)``;
